0916d7d64ca9b5712c18011e1c24c97d4bdede26
Ali-Raza <araza@cobrafiringsystems.com>
2022-11-24 19:50:29 +0500

    update API key

--- config/constants.php
+++ config/constants.php
@@ -7,7 +7,7 @@ return [
     |----------------------------------------------------------------------------
     */
     "open_api_base_url" => "https://api.openai.com/v1",
-    "open_api_auth" => "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",
+    "open_api_auth" => "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",
 
 //    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
 ];
